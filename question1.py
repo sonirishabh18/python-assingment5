48cccefab379786b0cac53b716de2c9d38ecbf23
sonirishabh18 <sonyrishabh18@gmail.com>
2026-06-23 23:29:39 +0530

    Create question1.py

---
+++ question1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.random.randint(1,101,20)
+
+print(a)
+print("Minimum =", np.min(a))
+print("Maximum =", np.max(a))
+print("Mean =", np.mean(a))
+print("Standard Deviation =", np.std(a))
